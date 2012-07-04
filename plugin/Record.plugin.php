b2bf64e91c540bfc56aaed1479fa0cf5d27264dd
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2012-07-05 00:12:21 +0200

    Record plugin
    This plugin allows to record entities paths and then playing it

---
+++ plugin/Record.plugin.php
@@ -0,0 +1,49 @@
+<?php
+
+
+class RecordPath{
+	protected $eid, $start, $path, $event, $client;
+	function __construct($client, $EID){
+		$this->eid = $EID;
+		$this->client = $client;
+		$this->start = Utils::microtime();
+		$this->event = $this->client->event("onEntityMove_".$EID, "onMove", $this);
+		$this->path = array();
+	}	
+	public function onMove($entity){
+		$coords = $entity->getPosition();
+		$this->path[] = array("time" => Utils::microtime() - $this->start, "coords" => array("x" => $coords["x"], "y" => $coords["y"], "z" => $coords["z"]));	
+	}
+	public function getPath(){
+		return $this->path;
+	}
+	public function stop(){
+		$this->client->deleteEvent("onEntityMove_".$this->eid, $this->event);
+	}
+}
+
+
+class PlayPath{
+	protected $start, $path, $event, $client;
+	function __construct($client, $path){
+		$this->client = $client;
+		$this->start = Utils::microtime();
+		$this->event = $this->client->event("onTick", "followPath", $this);
+		$this->path = $path;
+	}	
+	public function followPath($time, $event, $ob){
+		foreach($this->path as $i => $data){
+			if($data["time"] <= Utils::microtime() - $this->start){
+				$ob->move($data["coords"]["x"], $data["coords"]["y"], $data["coords"]["z"]);
+				unset($this->path[$i]);
+			}
+			break;
+		}
+		if(count($this->path) == 0){
+			$this->stop();
+		}
+	}
+	public function stop(){
+		$this->client->deleteEvent("onTick", $this->event);
+	}
+}
--- test.php
+++ test.php
@@ -8,20 +8,50 @@ define("ACTION_MODE", 1); //1 => ticks, other by packets.
 file_put_contents("console.log", "");
 file_put_contents("packets.log", "");
 $M = new MinecraftClient("127.0.0.1");
-$M->activateSpout();
-$M->event("onChat", "chatHandler");
-$M->event("onPluginMessage", "pluginViewer");
+//$M->activateSpout();
+$M->event("onChat", "testHandler");
+$M->event("onDeath", "testHandler");
+$M->event("onPluginMessage", "testHandler");
+
 $M->connect("BotAyuda", "");
 
-function pluginViewer($data, $event, $ob){
-	console("[*] Plugin Message: Channel => ".$data["channel"].", Data: ".$data["data"]);
- }
-
-function chatHandler($message, $event, $ob){
-	$m = explode(" ",$message);
-	if(array_pop($m) == "1234"){
-		$ob->say("Adios, mundo cruel!");
-		$ob->logout();
+function testHandler($message, $event, $ob){
+	global $record, $play;
+	switch($event){
+		case "onChat":
+			$m = explode(" ",$message);
+			$c = array_pop($m);
+			if($c == "1234"){
+				$ob->say("Adios, mundo cruel!");
+				$ob->logout();
+			}elseif($c == "4321"){
+				include_once("plugin/Record.plugin.php");
+				$eid = $ob->getPlayer("shoghicp")->getEID();
+				$record = new RecordPath($ob, $eid);
+			}elseif($c == "4444"){
+				if(isset($record)){
+					$record->stop();
+					$path = $record->getPath();
+					$play = new PlayPath($ob, $path);
+				}
+			}elseif($c == "5555"){
+				if(isset($play)){
+					$play->stop();
+				}
+			}
+			break;
+		case "onPluginMessage":
+			console("[INFO] Plugin Message: Channel => ".$data["channel"].", Data: ".$data["data"]);
+			break;
+		case "onDeath":
+			$messages = array(
+				"Nooo!!!",
+				"Por que??",
+				"Solo hice lo que me pedian!",
+				"Noooouuu!",			
+			);
+			$ob->say($messages[mt_rand(0,count($messages)-1)]);
+			break;
 	}
 }
 
