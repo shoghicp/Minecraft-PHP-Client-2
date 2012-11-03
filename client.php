f5b925d5353c9c5de7aaf45fe567d57169f5986d
Shoghi Cervantes Pueyo <shoghicp@gmail.com>
2012-11-03 14:22:55 +0100

    Updated license to WTFPL

--- client.php
+++ client.php
@@ -21,10 +21,19 @@
          
 	by @shoghicp
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+			DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+				Version 2, December 2004
+
+Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+Everyone is permitted to copy and distribute verbatim or modified
+copies of this license document, and changing it is allowed as long
+as the name is changed.
+
+			DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+	TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+	0. You just DO WHAT THE FUCK YOU WANT TO.
 
 
 */
@@ -75,10 +84,19 @@ echo <<<INFO
          
 \tby @shoghicp
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+			DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+				Version 2, December 2004
+
+Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+Everyone is permitted to copy and distribute verbatim or modified
+copies of this license document, and changing it is allowed as long
+as the name is changed.
+
+			DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+	TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+	0. You just DO WHAT THE FUCK YOU WANT TO.
 
 
 INFO;
